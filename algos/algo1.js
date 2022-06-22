b70ead4674ce70c08f29309f3d4a215448a02274
Michael <67665934+mjstafford@users.noreply.github.com>
2022-06-22 09:07:28 -0700

    algo update

---
+++ algos/algo1.js
@@ -0,0 +1,12 @@
+/*
+DATA TYPES
+
+data types can include the following:
+  1. Numbers -> integers and decimals (floats)
+  2. Strings -> characters and letters combined into one using quotes ->"Welcome to we fundamentals"
+  3. Booleans -> true or false
+*/
+
+let myName = "michael"
+
+console.log("Wazzaaa" + " " + myName)
